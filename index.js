dfd9a062414f844e2d3d5eb733dcda9464032b00
DanielGrenehed <daniel.grenehed97@gmail.com>
2024-02-13 00:04:10 +0200

    pretty much functional

--- index.js
+++ index.js
@@ -3,21 +3,47 @@ const io = require("socket.io-client");
 const assert = require("assert");
 
 const config = require("./config.json");
+
+let default_video_source = "";
+let video_key_map = new Map();
+function loadVideoSources() {
+	assert(config.hasOwnProperty("video_sources"));
+	console.log("Reading video_sources from config:");
+	let vsi = 0;
+	for (let video_source of config.video_sources) {
+		console.log(`video_source ${vsi++} : ${JSON.stringify(video_source)}`);
+		assert(video_source.hasOwnProperty("name"));
+		assert(video_source.hasOwnProperty("key"));
+		video_key_map[video_source.name] = video_source.key;
+	}
+	if (config.hasOwnProperty("default_video_source")) {
+		default_video_source = config.default_video_source;
+	} else if (video_key_map.keys().length > 0) {
+		default_video_source = video_key_map.keys()[0];
+	}
+}
+loadVideoSources();
+
 let meter_ids = [];
 let audio_sources = new Map();
 
-assert(config.hasOwnProperty("audio_sources"));
-console.log("Reading audio_source from config:");
-let asi = 0;
-for (let audio_source of config.audio_sources) {
-	console.log(`audio_source ${asi++}`);
-	assert(audio_source.hasOwnProperty("meter_id"));
-	assert(audio_source.hasOwnProperty("min_vol"));
-	let id = audio_source.meter_id;
+function loadAudioSources() {
+	assert(config.hasOwnProperty("audio_sources"));
+	console.log("Reading audio_source from config:");
+	let asi = 0;
+	for (let audio_source of config.audio_sources) {
+		console.log(`audio_source ${asi++} : ${JSON.stringify(audio_source)}`);
+		assert(audio_source.hasOwnProperty("meter_id"));
+		assert(audio_source.hasOwnProperty("min_vol"));
+		assert(audio_source.hasOwnProperty("video_source"));
+		let id = audio_source.meter_id;
 
-	meter_ids.push(id);
-	audio_sources[id] = audio_source;
+		meter_ids.push(id);
+		audio_sources[id] = audio_source;
+	}
 }
+loadAudioSources();
+
 
 class SumBuffer {
 	constructor() {
@@ -41,9 +67,11 @@ class SumBuffer {
 
 assert(config.hasOwnProperty("vmix"));
 const vmix = new Connection(config.vmix);
+let vmix_connected = false;
 console.log(`trying to connect to '${config.vmix}'`);
 
 vmix.on('connect', () => {
+	vmix_connected = true;
 	console.log(`vmix(${config.vmix}) connected!`);
 //	vmix.send('XML');
 });
@@ -51,14 +79,28 @@ vmix.on('XML', (data) => {
 	console.log(data);
 }); 
 vmix.on('close', () => {
+	vmix_connected = false;
 	console.log("Vmix connect failed");
 });
+
+function setVideoSource(video_source) {
+	let key = video_key_map[video_source];
+	if (key == undefined) {
+		console.log(`Could not set source to '${video_source}', no corresponding key`);
+		return;
+	}
+	if (!vmix_connected) {
+		console.log(`VMIX not connected, cannot change video source! (tried to set ${video_source})`);
+		return;
+	}
+	vmix.send({Input:key});
+}
 
 assert(config.hasOwnProperty("cumquat"));
 const socket = io(config.cumquat);
 console.log(`trying to connect to '${config.cumquat}'`);
 
-let meter_buffers = new Map();
+let meter_buffers = {};
 
 function onMeterUpdate(meters) {
 	for (let id of meter_ids) {
@@ -66,7 +108,7 @@ function onMeterUpdate(meters) {
 		if (!(id in meter_buffers)) {
 			meter_buffers[id] = new SumBuffer();
 		}
-		meter_buffers[id].push(parseFloat(meters[id]));
+		meter_buffers[id].push(parseFloat(meters[id].val));
 	}
 }
 
@@ -74,7 +116,9 @@ socket.on("connect", () => {
 	console.log(`cumquat connected (${config.cumquat}) (id: ${socket.id})`);
 
 	socket.on("mixerMeters", (arg) => {
-		onMeterUpdate(arg);
+		//console.log("mixerMeters");
+		//console.log(arg[1]);
+		onMeterUpdate(arg[1]);
 	});
 
 });
@@ -83,39 +127,27 @@ socket.on("close", () => {
 	console.log("Socketio connect failed");
 });
 
-// socket.emit("channel", "arg");
-
 function updateVideoSource() {
-	//console.log("updateVideoSource");
 	let valid_sources = [];
 	for (let id in meter_buffers) {
 		const avg = meter_buffers[id].getAverage();
 		if (avg > audio_sources[id].min_vol) {
-			console.log(`[${id}], ${avg}`);
 			valid_sources.push({id: id, avg: avg});
 		}			
 	}
 	
 	let loudest_source = undefined;
+	valid_sources.map((as) => {
+		if (loudest_source == undefined || as.avg > loudest_source.avg) {
+			loudest_source = as;
+		} 
+	});
 
-
+	console.log(`loudest source: ${JSON.stringify(loudest_source)}`);
+	let video_source = loudest_source == undefined ? default_video_source : audio_sources[loudest_source.id].video_source;
+	setVideoSource(video_source);
 }
 
-/*
- *	listen to sources from cumquat
- *	minimum strength 
- *	then when highest for long enough
- *	change vmix source to corresponding camera
- *	store time since last camera change, and 
- *	do not allow the camera to change too rapidly.
- *
- *	at an interval, list all inputs that have 
- *	an average surpassing their min-volume, 
- *	then get the one with the highest average
- *	volume. get that inputs corresponding camera,
- *	and set it as the video input in vmix
- *
- * */
 assert(config.hasOwnProperty("switch_delay_ms"));
 setInterval(updateVideoSource, config.switch_delay_ms);
 
